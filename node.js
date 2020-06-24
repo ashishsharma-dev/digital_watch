d1c9e158f6f87c6cc520a713759b628ba3c1b7a7
ashishsharma-dev <55922529+ashishsharma-dev@users.noreply.github.com>
2020-06-24 14:24:48 +0530

    I don't want any changes to this
    This is a JavaScript based digital watch app

---
+++ node.js
@@ -0,0 +1,169 @@
+const http = require('http');
+
+const hostname ='127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    // res.end('Ashish Sharma');
+    res.end(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Digital Watch</title>
+    </head>
+    <style>
+    * {margin: 0;padding: 0;box-sizing: border-box;}
+    body{font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;}
+    
+    .container {
+        display: flex;
+        width: 100%;
+        height: 100vh;
+        background: linear-gradient(to right, crimson, royalblue, gold);
+        justify-content: center;
+        align-items: center;
+    }
+    
+    .main{
+        display: grid;
+        width: 80%;
+    
+        grid-template-columns: repeat(3, 1fr);
+        grid-gap: 20px;
+        color: #fff;
+        font-size: 40px;
+        border: 10px solid #eee;
+        padding: 20px;
+    }
+    .main > div {
+        background: #282828;
+        padding: 10px;
+        justify-content: center;
+        display: flex;
+        align-items: center;
+    }
+    
+    .main .hours {
+        grid-row: 1 / 3;
+        font-size: 60px;
+        font-weight: 600;
+    }
+    
+    .main .quotes {
+        font-size: 30px;
+        grid-column: 1/4;
+        height: 100px;
+    }
+
+    </style>
+    <body>
+        <div class="container">
+            <div class="main">
+                <div class="hours"></div>
+                <div class="minutes"></div>
+                <div class="seconds"></div>
+                <div class="day"></div>
+                <div class="greet"></div>
+                <div class="quotes"></div>
+            </div>
+        </div>
+        <script>
+            
+setInterval(myApp, 500);
+
+function myApp () {
+
+    let myTime = new Date();
+
+    let hr = myTime.getHours();
+    let min = myTime.getMinutes();
+    let sec = myTime.getSeconds();
+    let dy = myTime.getDay();
+
+    let hoursDOM = document.querySelector('.hours');
+    let minutesDOM = document.querySelector('.minutes');
+    let secondsDOM = document.querySelector('.seconds');
+    let dayDOM = document.querySelector('.day');
+
+    hoursDOM.innerHTML = hr;
+    minutesDOM.innerHTML = min;
+    secondsDOM.innerHTML = sec;
+
+    minutesDOM.append(' Minutes')
+    secondsDOM.append(' Seconds')
+
+
+    if (hr < 10){
+        hoursDOM.innerHTML = '0' + hr + ' Hours';
+    } else if (min < 10){
+        minutesDOM.innerHTML = '0' + min + ' Minutes';
+    } else if (sec < 10) {
+        secondsDOM.innerHTML = '0' + sec + ' Seconds';
+    }
+
+    if (hr > 12) {
+        hoursDOM.append(' PM');
+    }else {
+        hoursDOM.append(' AM');
+    }
+
+
+    if (dy == 0) {
+        dayDOM.innerHTML = 'Sunday';
+    } else if (dy == 1) {
+        dayDOM.innerHTML = 'Monday';
+    } else if (dy == 2) {
+        dayDOM.innerHTML = 'Tuesday';
+    } else if (dy == 3) {
+        dayDOM.innerHTML = 'Wednesday';
+    } else if (dy == 4) {
+        dayDOM.innerHTML = 'Thursday';
+    } else if (dy == 5) {
+        dayDOM.innerHTML = 'Friday';
+    } else if (dy == 6) {
+        dayDOM.innerHTML = 'Saturday';
+    }
+
+        
+    function timeGreet () {
+        let greetings = document.querySelector('.greet');
+
+        if (hr < 12) {
+            greetings.innerHTML = 'Good Morning!';
+        } else if (hr >= 12 && hr < 16) {
+            greetings.innerHTML = 'Good Afternoon!';
+        } else {
+            greetings.innerHTML = 'Good Evening!'
+        }
+    }
+
+    timeGreet();
+}
+
+
+function random () {
+
+    let quotesDOM = document.querySelector('.quotes');
+
+    let randomQuotes = ["All our dreams can come true, if we have the courage to pursue them",
+    "The secret of getting ahead is getting started.", "It's hard to beat a person who never gives up.", "Whatever you are, be a good one."];
+
+    let randomPrints = Math.floor(Math.random() * randomQuotes.length);
+
+    quotesDOM.innerHTML = randomQuotes[randomPrints];
+}
+
+setInterval(random, 10000);
+
+        </script>
+    </body> 
+    </html>`);
+});
+
+server.listen(port, hostname, () => {
+    console.log(`server running at http://${hostname}:${port}/`)
+});
